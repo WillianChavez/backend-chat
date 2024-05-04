d7e7f8423a4f2eb5a18f37cb44d0c639e000ad8a
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2024-05-04 12:33:08 -0600

    Add AuthDto for user authentication

---
+++ src/auth/dto/auth-dto.ts
@@ -0,0 +1,48 @@
+import { IsNotEmpty, IsString } from "class-validator";
+/**
+ * @module auth/dto/AuthDto
+ * 
+ * @description DTO para la autenticación de usuarios
+ * 
+ * @property {string} username - Nombre de usuario
+ * @property {string} password - Contraseña del usuario
+ * 
+ * @returns {AuthDto} - DTO para la autenticación de usuarios
+ * 
+ * @example
+ * {
+ *   "username": "usuario",
+ *   "password": "contraseña"
+ * }
+ * 
+ */
+export class AuthDto {
+  /**
+   * @description Nombre de usuario
+   * @var {string} username
+   */
+  @IsString({
+    message: 'El nombre de usuario debe ser una cadena de texto.'
+  })
+  @IsNotEmpty({
+    message: 'El nombre de usuario es requerido.',
+    context: {
+      targetName: 'Usuario',
+      object: 'auth',
+      property: 'username'
+    }
+  })
+  username: string;
+
+  /**
+   * @description Contraseña del usuario
+   * @var {string} password
+   */
+  @IsString({
+    message: 'La contraseña debe ser una cadena de texto.'
+  })
+  @IsNotEmpty({
+    message: 'La contraseña es requerida.'
+  })
+  password: string;
+}

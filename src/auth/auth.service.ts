b32f543784b202899de79af127fa38f66afcf5ca
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2024-05-03 21:47:18 -0600

    feat: add authentication service with bcrypt password comparison
    The `auth` method in the `AuthService` class has been updated to include password comparison using bcrypt. This ensures secure authentication by comparing the entered password with the stored password in the database.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import Usuario from 'src/common/database/models/usuario.model';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
 
-  auth(user: string, password: string): boolean {
+  /*
+   * Esta función se encarga de autenticar a un usuario
+   * Recibe como parámetros el nombre de usuario y la contraseña
+   * Devuelve un booleano que indica si la autenticación fue exitosa o no
+   */
+  async auth(user: string, password: string): Promise<boolean> {
+    /*
+     * Buscar el usuario en la base de datos
+     */
+    const usuario = await Usuario.findOne({
+      where: {
+        nombre: user,
+      }
+    });
 
-    return true;
+    /*
+     * Si el usuario no existe, la función devolverá false
+     */
+    if (!usuario) return false;
+
+    /*
+     * Comparar la contraseña ingresada con la contraseña almacenada en la base de datos
+     * Si las contraseñas coinciden, la función devolverá true
+     */
+    return await bcrypt.compare(password, usuario.contra)
   }
 }

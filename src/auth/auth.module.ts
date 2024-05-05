ddd931b5a8d9a755d71eaf6a0dd49c10be84fec2
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2024-05-05 00:26:31 -0600

    feat: agregar importación de SequelizeModule y JwtService en AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 // import { AuthController } from './auth.controller';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import Usuario from 'src/common/database/models/usuario.model';
+import DispositivoVinculado from 'src/common/database/models/dispositivo-vinculado.model';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-
+  imports: [SequelizeModule.forFeature([
+    Usuario,
+    DispositivoVinculado
+  ])],
   controllers: [AuthController],
-
-  providers: [AuthService]
+  providers: [AuthService, JwtService],
 })
-export class AuthModule {}
+export class AuthModule { }

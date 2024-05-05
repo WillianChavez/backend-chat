d7815803d4a3d29b84461ece3f524b01526de0b0
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2024-05-04 18:42:55 -0600

    feat: add user module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CommonModule, ChatModule],
+  imports: [CommonModule, ChatModule, UserModule],
   controllers: [],
   providers: [],
 })
